69ac1908a8a01b85337a76a84f70585f386fe61e
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-23 11:46:00 +0530

    added missing repository annotation for task

--- server/src/main/java/com/taskmanager/server/repository/TaskRepository.java
+++ server/src/main/java/com/taskmanager/server/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.taskmanager.server.repository;
 
-public class TaskRepository {
+import com.taskmanager.server.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository <Task, Long> {
 }
