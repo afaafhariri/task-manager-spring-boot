aec44dadc06b6ed6b025c36f8c04c0720ce01bd3
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-23 11:26:05 +0530

    initialization

---
+++ server/src/main/java/com/taskmanager/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
