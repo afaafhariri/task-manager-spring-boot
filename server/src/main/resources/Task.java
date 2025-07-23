efebc3b769d1fb57f98cc7e07136b00ca060b8eb
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-23 14:27:30 +0530

    fixed missing class

--- server/src/main/resources/Task.java
+++ server/src/main/resources/Task.java
@@ -4,20 +4,16 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "tasks")
+@Table(name="tasks")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Task {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
-
     private String description;
-
     private Boolean completed = false;
 }
