ac96dbf3bac46d0e83da92099a3de883744c7fff
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-23 14:33:58 +0530

    fetch one tasl

--- server/src/main/java/com/taskmanager/server/controller/TaskController.java
+++ server/src/main/java/com/taskmanager/server/controller/TaskController.java
@@ -27,4 +27,12 @@ public class TaskController {
         return ResponseEntity.ok(tasks);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
 }
