66d8c21edd71f515c554c6bfd31e035b3b08edca
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-23 12:08:26 +0530

    task service implementation class

---
+++ server/src/main/java/com/taskmanager/server/service/TaskServiceImpl.java
@@ -0,0 +1,10 @@
+package com.taskmanager.server.service;
+
+import com.taskmanager.server.model.Task;
+import com.taskmanager.server.repository.TaskRepository;
+import com.taskmanager.server.service.TaskService;
+import java.util.List;
+import java.util.Optional;
+
+public class TaskServiceImpl {
+}
