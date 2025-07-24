112303b9a50a4b7cc4cc7b3fb9e62173e7eabc79
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-25 01:46:53 +0530

    user specific tabs

--- server/src/main/java/com/taskmanager/server/repository/TaskRepository.java
+++ server/src/main/java/com/taskmanager/server/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.taskmanager.server.repository;
 
 import com.taskmanager.server.model.Task;
+import com.taskmanager.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository <Task, Long> {
+    List<Task> findByUser(User user);
+    Optional<Task> findByIdAndUser(Long id, User user);
+    void deleteByUser(User user);
 }
